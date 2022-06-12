c8ae8617315d2cbeb4941163f65118d418e1d905
cavdarturgay <turgaycavdar@gmail.com>
2022-06-12 11:51:00 +0300

    hangman added

--- hangman.py
+++ hangman.py
@@ -87,11 +87,10 @@ while not end_of_game:
             display[position] = letter
     if guess not in chosen_word:
       lives = lives - 1
-      #print(lives)
-      print(stages[lives])
-    if lives == 0:
-      print("You Lost!!!")
-      exit()
+      if lives == 0:
+        end_of_game = True
+        print("You Lost!!!")
+      
 
     #TODO-2: - If guess is not a letter in the chosen_word,
     #Then reduce 'lives' by 1. 
@@ -105,4 +104,4 @@ while not end_of_game:
         print("You win.")
 
     #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
-    
+    print(stages[lives])
